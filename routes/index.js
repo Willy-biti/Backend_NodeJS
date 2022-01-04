5688452470541bab526e0d7f65838ff70f1bc328
Willy Biti <willy.biti@gmail.com>
2022-01-04 19:13:02 -0300

    API

--- routes/index.js
+++ routes/index.js
@@ -3,71 +3,71 @@ var router = express.Router();
 var Pessoa = require('../modelos/pessoa');
 
 router.get('/', function(request, response, next) {
-Pessoa.todos(function(pessoas) {
+  Pessoa.todos(function(pessoas) {
     response.render('index', { 
-    title: 'Node.js com framework express',
-    pessoas: pessoas
+      title: 'Node.js com framework express',
+      pessoas: pessoas
     });
-});
+  });
 });
 
 router.get('/alterar', function(request, response, next) {
-Pessoa.buscar(request.query.cpf, function(pessoa) {
+  Pessoa.buscar(request.query.cpf, function(pessoa) {
     if (pessoa == null) { 
-    console.log("Pessoa não encontrada");
-    response.render('alterar', {'pessoa': {}})
+      console.log("Pessoa não encontrada");
+      response.render('alterar', {'pessoa': {}})
     }
     else{
-    response.render('alterar', {'pessoa': pessoa})
+      response.render('alterar', {'pessoa': pessoa})
     }
-});
+  });
 });
 
 router.post('/alterar-pessoa', function(request, response, next) {
-var pessoa = new Pessoa();
+  var pessoa = new Pessoa();
 
-pessoa.cpf        = request.body.cpf;
-pessoa.nome       = request.body.nome;
-pessoa.sobrenome  = request.body.sobrenome;
-pessoa.telefone   = request.body.telefone;
-pessoa.endereco   = request.body.endereco;
+  pessoa.cpf        = request.body.cpf;
+  pessoa.nome       = request.body.nome;
+  pessoa.sobrenome  = request.body.sobrenome;
+  pessoa.telefone   = request.body.telefone;
+  pessoa.endereco   = request.body.endereco;
 
-pessoa.salvar(function(){
+  pessoa.salvar(function(){
     response.redirect("/");
-}, request.query.cpfAterar)
+  }, request.query.cpfAterar)
 });
 
 
 router.get('/excluir', function(request, response, next) {
-var pessoa = new Pessoa();
-pessoa.cpf = request.query.cpf;
-pessoa.excluir(function(pessoas){
+  var pessoa = new Pessoa();
+  pessoa.cpf = request.query.cpf;
+  pessoa.excluir(function(pessoas){
     response.redirect("/");
-})
+  })
 
 });
 
 router.get('/pesquisar', function(request, response, next) {
-Pessoa.buscarPorNome(request.query.nome, function(pessoas) {
+  Pessoa.buscarPorNome(request.query.nome, function(pessoas) {
     response.render('index', { 
-    title: 'Pesquisando em arquivos', 
-    pessoas: pessoas
+      title: 'Pesquisando em arquivos', 
+      pessoas: pessoas
     });
-});
+  });
 });
 
 router.post('/cadastrar-pessoa', function(request, response, next) {
-var pessoa = new Pessoa();
+  var pessoa = new Pessoa();
 
-pessoa.cpf        = request.body.cpf;
-pessoa.nome       = request.body.nome;
-pessoa.sobrenome  = request.body.sobrenome;
-pessoa.telefone   = request.body.telefone;
-pessoa.endereco   = request.body.endereco;
+  pessoa.cpf        = request.body.cpf;
+  pessoa.nome       = request.body.nome;
+  pessoa.sobrenome  = request.body.sobrenome;
+  pessoa.telefone   = request.body.telefone;
+  pessoa.endereco   = request.body.endereco;
 
-pessoa.salvar(function(){
+  pessoa.salvar(function(){
     response.redirect("/");
-});
+  });
 });
 
 module.exports = router;
